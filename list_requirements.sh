ba377785d4a1201ae4e857adb3f3f029abd07989
Joel Capitao <jcapitao@redhat.com>
2022-02-16 14:16:42 +0100

    list requirements

---
+++ list_requirements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf .venv
+virtualenv -p /usr/bin/python3.9 .venv
+source .venv/bin/activate
+
+pip install rdopkg pymod2pkg
+rdopkg info conf:rpmfactory-client | grep name: | awk '{print $2}' > openstack_clients
+
+> dependencies
+while read client; do
+	pkg_name=$(pymod2pkg $client --pyver py3)
+	echo -e "# $pkg_name" | tee -a dependencies
+	sudo dnf install $pkg_name <<< 'no' | grep -e delorean -e epel | tee -a dependencies
+	echo -e "" >> dependencies
+done < openstack_clients
